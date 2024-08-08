ccd69637e5bc2c2fcfacdb553f7507c634787b1f
Kyle <kylcheng@gmail.com>
2024-08-08 17:29:25 +0100

    chore(tasks): working task update/saves + fix taskName save

--- lib/services/data/tasks/widgets/task_form/task_name_field.dart
+++ lib/services/data/tasks/widgets/task_form/task_name_field.dart
@@ -13,23 +13,43 @@ class TaskNameField extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final curTaskName = ref.watch(curTaskProvider.select((state) => state.task.name));
+    final curTaskName =
+        ref.watch(curTaskProvider.select((state) => state.task.name));
     final nameController = useTextEditingController(text: curTaskName);
+    final focusNode = useFocusNode();
 
-    // Must manually update the controller's text when the task name changes    
+    // Must manually update the controller's text when the task name changes
     useEffect(() {
-    nameController.text = curTaskName;
-    return null;
-  }, [curTaskName]);
-    
+      nameController.text = curTaskName;
+      return null;
+    }, [curTaskName]);
+
+    // HACK to get textController to sync with curTaskProvider
+    // The onSubmitted event is never called
+    // And updating state onChanged will reset the TextField
+
+    // TODO: p3 - should remove hack and find better solution 
+    // Current approach will NOT work with keyboards and button is pressed, as submit event is never triggered 
+
+    focusNode.addListener(() {
+      if (!focusNode.hasFocus && nameController.text != curTaskName) {
+        ref.read(curTaskProvider.notifier).updateTaskName(nameController.text);
+        FocusScope.of(context).unfocus(); // Hide the keyboard
+      }
+    });
+
     return TextField(
+      focusNode: focusNode,
       controller: nameController,
       enabled: enabled,
-        
+      textInputAction: TextInputAction.done,
+      onSubmitted: (value) {
+        ref.read(curTaskProvider.notifier).updateTaskName(value);
+        FocusScope.of(context).unfocus(); // Hide the keyboard
+      },
       onEditingComplete: () {
-        ref.read(curTaskProvider.notifier).updateTask(
-          ref.read(curTaskProvider).task.copyWith(name: nameController.text)
-        );
+        ref.read(curTaskProvider.notifier).updateTaskName(nameController.text);
+        FocusScope.of(context).unfocus(); // Hide the keyboard
       },
       decoration: InputDecoration(
         hintText: 'Enter task name',
