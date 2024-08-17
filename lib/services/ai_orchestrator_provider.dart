6e6e940a4044c8b9d5b0997ebe6dbe133f4b25dc
Kyle <kylcheng@gmail.com>
2024-08-17 22:58:38 +0100

    feat(ai_orchestrator): initial POC - animated task field changes

--- lib/services/ai_orchestrator_provider.dart
+++ lib/services/ai_orchestrator_provider.dart
@@ -3,8 +3,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:seren_ai_flutter/constants.dart';
+import 'package:seren_ai_flutter/services/data/projects/models/project_model.dart';
 import 'package:seren_ai_flutter/services/data/tasks/cur_task_provider.dart';
+import 'package:seren_ai_flutter/services/data/tasks/models/joined_task_model.dart';
+import 'package:seren_ai_flutter/services/data/tasks/models/task_model.dart';
 import 'package:seren_ai_flutter/services/data/tasks/widgets/task_page.dart';
+import 'package:seren_ai_flutter/services/data/teams/models/team_model.dart';
+import 'package:seren_ai_flutter/services/data/users/models/user_model.dart';
 
 final isAiEditingProvider = StateProvider<bool>((ref) => false);
 
@@ -26,14 +31,54 @@ class AiOrchestrator {
 
     print('openTaskPage done');
 
-    // wait for 2 seconds since openTaskPage clears the task data 
+    
     await Future.delayed(Duration(milliseconds: 250));
-    ref.read(curTaskProvider.notifier).updateTaskName('AI Set Task Test');
 
-        
+    final joinedTask = JoinedTaskModel(
+      task: TaskModel(
+        name: 'AI Set Task Test',
+        dueDate: DateTime.now(),
+        parentProjectId: 'parentProjectId',
+        parentTeamId: 'parentTeamId',
+        description: 'Task description',
+        statusEnum: StatusEnum.inProgress,
+        createdDate: DateTime.now(),
+        lastUpdatedDate: DateTime.now(),
+        authorUserId: 'authorUserId',
+      ),
+      authorUser: UserModel(
+        id: 'authorUserId',        
+        email: 'ai@seren.ai',
+        parentAuthUserId: 'parentAuthUserId',
+      ),
+      project: ProjectModel(
+        id: 'projectId',
+        name: 'TEST',
+        description: 'test',
+        parentOrgId: 'parentOrgId',
+        parentTeamId: 'parentTeamId',
+      ),
+      team: TeamModel(
+        id: 'teamId',
+        name: 'TEST',
+        parentOrgId: 'parentOrgId',
+      ),
+      assignees: [
+        UserModel(
+          id: 'assigneeUserId',
+          email: 'ai@seren.ai',
+          parentAuthUserId: 'parentAuthUserId',
+        ),
+      ],
+      comments: [],
+    );
+
+    ref.read(curTaskProvider.notifier).updateAllFields(joinedTask);
 
 
-    test(); 
+  
+
+    //test(); 
 
     // Delay setting isAiEditingProvider to false to ensure animation is triggered
     await Future.delayed(Duration(milliseconds: 500));
@@ -50,7 +95,7 @@ class AiOrchestrator {
 
 
     // TODO: how to animate/highlight the taskName change 
-    ref.read(curTaskProvider.notifier).updateTaskName('AI Set Task Test');
+    //ref.read(curTaskProvider.notifier).updateTaskName('AI Set Task Test');
 
     print('test');
   }
