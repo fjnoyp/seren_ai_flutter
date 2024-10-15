0d3ce1d1a7db3700f64a88c906dc15adb918c59f
renata-sisilio <renata@seren-ai.co>
2024-10-15 19:22:57 -0300

    fix(navigation): add popscope to prevent pop from initial page
    fix: remove duplicated unused main scaffold widget
    
    fix(auth): solve guard crashing problem
    
    refactor: solve widget build issues on state when pop

--- lib/widgets/common/main_scaffold.dart
+++ lib/widgets/common/main_scaffold.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:seren_ai_flutter/constants.dart';
 import 'package:seren_ai_flutter/services/ai_interaction/widgets/user_input_display_widget.dart';
 import 'drawer_view.dart';
 
@@ -21,74 +22,83 @@ class MainScaffold extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return Scaffold(
-      backgroundColor: theme.scaffoldBackgroundColor,
-      appBar: AppBar(
-        backgroundColor: theme.appBarTheme.backgroundColor,
-        elevation: 0,
-        leading: Builder(
-          builder: (BuildContext context) {
-            return Row(
-              children: [
-                IconButton(
-                  icon: Icon(Icons.menu, color: theme.iconTheme.color),
-                  onPressed: () {
-                    Scaffold.of(context).openDrawer();
-                  },
-                ),
-                // TODO p2: user can pop out to nothing on the starting page ... back button is in a weird location. we should only conditionally show back button
-                if (Navigator.of(context).canPop())
+    return PopScope(
+      canPop: Navigator.of(context).canPop(),
+      onPopInvokedWithResult: (didPop, result) {
+        if (!didPop) {
+          Navigator.of(context)
+              .pushNamedAndRemoveUntil(homeRoute, (route) => false);
+        }
+      },
+      child: Scaffold(
+        backgroundColor: theme.scaffoldBackgroundColor,
+        appBar: AppBar(
+          backgroundColor: theme.appBarTheme.backgroundColor,
+          elevation: 0,
+          leading: Builder(
+            builder: (BuildContext context) {
+              return Row(
+                children: [
                   IconButton(
-                    icon: Icon(Icons.arrow_back, color: theme.iconTheme.color),
-                    onPressed: () => Navigator.of(context).pop(),
+                    icon: Icon(Icons.menu, color: theme.iconTheme.color),
+                    onPressed: () {
+                      Scaffold.of(context).openDrawer();
+                    },
                   ),
-              ],
+                  // TODO p2: back button is in a weird location. we should only conditionally show back button
+                  if (Navigator.of(context).canPop())
+                    IconButton(
+                    icon: Icon(Icons.arrow_back, color: theme.iconTheme.color),
+                      onPressed: () => Navigator.of(context).pop(),
+                    ),
+                ],
+              );
+            },
+          ),
+          leadingWidth: Navigator.of(context).canPop()
+              ? 96
+              : 48, // Adjust width based on whether back button is shown
+
+          title: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              title,
+              style: theme.appBarTheme.titleTextStyle,
+            ),
+          ),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.more_vert, color: theme.iconTheme.color),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        drawer: const DrawerView(),
+        body: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(0),
+              child: body,
+            ),
+          ],
+        ),
+
+        /*
+        floatingActionButton: enableAiBar ? Consumer(
+          builder: (context, ref, child) {
+            return FloatingActionButton(
+              onPressed: () {
+                ref.read(aiOrchestratorProvider).testMove(context);
+              },
+              child: Icon(Icons.pets),
             );
           },
-        ),
-        leadingWidth: Navigator.of(context).canPop()
-            ? 96
-            : 48, // Adjust width based on whether back button is shown
+        ) : null,
+        */
 
-        title: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(
-            title,
-            style: theme.appBarTheme.titleTextStyle,
-          ),
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.more_vert, color: theme.iconTheme.color),
-            onPressed: () {},
-          ),
-        ],
+        bottomNavigationBar:
+            showBottomBar ? const UserInputDisplayWidget() : null,
       ),
-      drawer: const DrawerView(),
-      body: Stack(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(0),
-            child: body,
-          ),
-          
-        ],
-      ),
-
-      /*
-      floatingActionButton: enableAiBar ? Consumer(
-        builder: (context, ref, child) {
-          return FloatingActionButton(
-            onPressed: () {
-              ref.read(aiOrchestratorProvider).testMove(context);
-            },
-            child: Icon(Icons.pets),
-          );
-        },
-      ) : null,
-      */
-
-      bottomNavigationBar: showBottomBar ? const UserInputDisplayWidget() : null,
     );
   }
 }
