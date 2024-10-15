edb3b45c3963bc04d4cadd21108da0e61ea18c66
renata-sisilio <renata@seren-ai.co>
2024-10-15 19:22:57 -0300

    fix(navigation): add popscope to prevent pop from initial page
    fix: remove duplicated unused main scaffold widget
    
    fix(auth): solve guard crashing problem
    
    refactor: solve widget build issues on state when pop

--- lib/services/auth/widgets/auth_guard.dart
+++ lib/services/auth/widgets/auth_guard.dart
@@ -14,19 +14,21 @@ class AuthGuard extends ConsumerWidget {
     final user = ref.watch(curAuthUserProvider);
 
     if (user == null) {
-      // TODO: replace with some runtime logic instead of just waiting 3 seconds
-      Future.delayed(const Duration(seconds: 3)).then(
+      // TODO: replace with some runtime logic instead of just waiting a second
+      Future.delayed(const Duration(seconds: 1)).then(
         (_) {
+          final user = ref.read(curAuthUserProvider);
           if (user == null) {
             // Redirect to sign-in page if user is not authenticated
             Navigator.of(context)
                 .pushNamedAndRemoveUntil(signInUpRoute, (route) => false);
-            return Container(); // Return an empty container while redirecting
           }
         },
       );
+    } else {
+      return child;
     }
-
-    return child;
+    
+    return Container(); // Return an empty container while redirecting
   }
 }
