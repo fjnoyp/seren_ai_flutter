1898691ac8a787c243537afd43e50c0c2479fd8e
renata-sisilio <renata@seren-ai.co>
2024-10-14 22:38:13 -0300

    fix(tasks): remove user assignments
    fix(tasks): enable empty user assignments

--- lib/services/data/tasks/task_user_assignments_db_provider.dart
+++ lib/services/data/tasks/task_user_assignments_db_provider.dart
@@ -1,10 +1,11 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:seren_ai_flutter/services/data/common/base_table_db.dart';
+import 'package:seren_ai_flutter/services/data/common/base_table_read_db.dart';
 import 'package:seren_ai_flutter/services/data/db_setup/db_provider.dart';
 import 'package:seren_ai_flutter/services/data/tasks/models/task_user_assignments_model.dart';
 
-final taskUserAssignmentsDbProvider = Provider<BaseTableDb<TaskUserAssignmentsModel>>((ref) {
-  return BaseTableDb<TaskUserAssignmentsModel>(
+final taskUserAssignmentsReadDbProvider =
+    Provider<BaseTableReadDb<TaskUserAssignmentsModel>>((ref) {
+  return BaseTableReadDb<TaskUserAssignmentsModel>(
     db: ref.watch(dbProvider),
     tableName: 'task_user_assignments',
     fromJson: (json) => TaskUserAssignmentsModel.fromJson(json),
