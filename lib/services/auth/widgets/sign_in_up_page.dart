f21644ddf39423f6eb2d234c6bd4d5642510fcf8
renata-sisilio <renata@seren-ai.co>
2024-10-16 20:16:00 -0300

    fix(auth): solve auth overriding user, use supabase sdk

--- lib/services/auth/widgets/sign_in_up_page.dart
+++ lib/services/auth/widgets/sign_in_up_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:seren_ai_flutter/constants.dart';
+import 'package:seren_ai_flutter/services/data/users/models/user_model.dart';
 import 'package:supabase_auth_ui/supabase_auth_ui.dart';
 
 class SignInUpPage extends StatelessWidget {
@@ -8,33 +9,45 @@ class SignInUpPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: Column(
-          children: [
-            SupaEmailAuth(
-              onSignInComplete: (response) {
-                Navigator.of(context).pushReplacementNamed(homeRoute);
-              },
-              onSignUpComplete: (response) {
-                Navigator.of(context).pushReplacementNamed(homeRoute);
-                // TODO p2: create entry in public.users table 
-              },
-              metadataFields: [
-                MetaDataField(
-                  prefixIcon: const Icon(Icons.person),
-                  label: 'Username',
-                  key: 'username',
-                  validator: (val) {
-                    if (val == null || val.isEmpty) {
-                      return 'Please enter something';
-                    }
-                    return null;
-                  },
-                ),
-              ],
-            )
-          ],
-        ),
+      padding: const EdgeInsets.all(24.0),
+      child: Column(
+        children: [
+          SupaEmailAuth(
+            onSignInComplete: (response) {
+              Navigator.of(context).pushReplacementNamed(homeRoute);
+            },
+            onSignUpComplete: (response) async {
+              if (response.user != null && response.user!.email != null) {
+                final user = response.user!;
+                await Supabase.instance.client.from('users').insert(
+                      UserModel(parentAuthUserId: user.id, email: user.email!)
+                          .toJson()
+                        ..addAll(
+                          {
+                            'created_at': DateTime.now().toIso8601String(),
+                            'updated_at': DateTime.now().toIso8601String()
+                          },
+                        ),
+                    );
+              }
+              Navigator.of(context).pushReplacementNamed(homeRoute);
+            },
+            metadataFields: [
+              MetaDataField(
+                prefixIcon: const Icon(Icons.person),
+                label: 'Username',
+                key: 'username',
+                validator: (val) {
+                  if (val == null || val.isEmpty) {
+                    return 'Please enter something';
+                  }
+                  return null;
+                },
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
