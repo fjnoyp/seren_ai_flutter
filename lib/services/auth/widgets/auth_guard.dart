07db3168a2e02825a2702c0ec0b07a5db014891d
Kyle <kylcheng@gmail.com>
2024-10-17 00:31:36 +0100

    fix(auth_guard): remove nested null user check breaking login

--- lib/services/auth/widgets/auth_guard.dart
+++ lib/services/auth/widgets/auth_guard.dart
@@ -14,21 +14,14 @@ class AuthGuard extends ConsumerWidget {
     final user = ref.watch(curAuthUserProvider);
 
     if (user == null) {
-      // TODO: replace with some runtime logic instead of just waiting a second
-      Future.delayed(const Duration(seconds: 1)).then(
-        (_) {
-          final user = ref.read(curAuthUserProvider);
-          if (user == null) {
-            // Redirect to sign-in page if user is not authenticated
-            Navigator.of(context)
-                .pushNamedAndRemoveUntil(signInUpRoute, (route) => false);
-          }
-        },
-      );
+      // Use addPostFrameCallback to navigate after the build is complete
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        Navigator.of(context)
+            .pushNamedAndRemoveUntil(signInUpRoute, (route) => false);
+      });
+      return Container(); // Return an empty container while redirecting
     } else {
       return child;
     }
-    
-    return Container(); // Return an empty container while redirecting
   }
 }
