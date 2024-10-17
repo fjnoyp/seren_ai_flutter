0ccf4d63228a9fe8ccef640701ef3e71c2e6e2cf
Kyle <kylcheng@gmail.com>
2024-10-17 15:16:09 +0100

    fix(projects): switch cur user viewable projects to a provider to avoid dependency update issues
    1. use providers when necessary as they don’t trigger complex state graph builds that might prevent an update from occuring
    
    2. avoid diamond dependencies where base and intermediate providers intermix and are used in a even more complex notifier provider

--- lib/services/data/projects/cur_user_viewable_projects_listener_provider.dart
+++ lib/services/data/projects/cur_user_viewable_projects_listener_provider.dart
@@ -4,45 +4,25 @@ import 'package:seren_ai_flutter/services/data/orgs/cur_org/cur_org_id_provider.
 import 'package:seren_ai_flutter/services/data/orgs/cur_org/is_cur_user_org_admin_listener_provider.dart';
 import 'package:seren_ai_flutter/services/data/projects/cur_user_projects_listener_provider.dart';
 import 'package:seren_ai_flutter/services/data/projects/models/project_model.dart';
+import 'package:seren_ai_flutter/services/data/projects/projects_listener_fam_provider.dart';
 
 final curUserViewableProjectsListenerProvider =
-    NotifierProvider<CurUserViewableProjectsListenerProvider, List<ProjectModel>?>(
-        CurUserViewableProjectsListenerProvider.new);
+    Provider<List<ProjectModel>?>((ref) {
+  final watchedIsCurUserOrgAdmin = ref.watch(isCurUserOrgAdminListenerProvider);
 
-class CurUserViewableProjectsListenerProvider extends Notifier<List<ProjectModel>?> {
-  @override
-  List<ProjectModel>? build() {
-    final watchedIsCurUserOrgAdmin =
-        ref.watch(isCurUserOrgAdminListenerProvider);
+  if (watchedIsCurUserOrgAdmin) {
+    final watchedCurOrgId = ref.watch(curOrgIdProvider);
 
-    if (watchedIsCurUserOrgAdmin) {
-      final watchedCurOrgId = ref.watch(curOrgIdProvider);
-      final query =
-          "SELECT * FROM projects WHERE parent_org_id = '$watchedCurOrgId'";
-
-      final db = ref.read(dbProvider);
-
-      final subscription = db.watch(query).listen((results) {
-        List<ProjectModel> items =
-            results.map((e) => ProjectModel.fromJson(e)).toList();
-        state = items;
-      });
-
-      // Cancel the subscription when the notifier is disposed
-      ref.onDispose(() {
-        subscription.cancel();
-      });
-    } else {
-      // get all projects that the current user has a role in
-      final userProjects = ref.watch(curUserProjectsListenerProvider);
-      if (userProjects != null) {
-        state = userProjects;
-      } else {
-        state = [];
-      }
+    if(watchedCurOrgId == null) {
+      return [];
     }
 
-    // Return the initial state
-    return null;
+    final allProjects = ref.watch(projectsListenerFamProvider(watchedCurOrgId));
+
+    return allProjects ?? [];
+  } else {
+    // get all projects that the current user has a role in
+    final userProjects = ref.watch(curUserProjectsListenerProvider);
+    return userProjects ?? [];
   }
-}
+});
