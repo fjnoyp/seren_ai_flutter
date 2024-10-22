dd143d1caca925e4356c04f9c3888c1a07b8a2b0
renata-sisilio <renata@seren-ai.co>
2024-10-22 08:20:17 -0300

    feat(shifts): integrate shift-related home cards

---
+++ lib/widgets/home/shift_home_card.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
+import 'package:seren_ai_flutter/constants.dart';
+import 'package:seren_ai_flutter/services/data/shifts/cur_shifts/cur_user_active_shift_ranges_fam_provider.dart';
+import 'package:seren_ai_flutter/services/data/shifts/cur_shifts/cur_user_joined_shifts_listener_provider.dart';
+import 'package:seren_ai_flutter/widgets/home/base_home_card.dart';
+
+class ShiftCard extends StatelessWidget {
+  const ShiftCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BaseHomeCard(
+      title: "Today's Shift",
+      color: Theme.of(context).colorScheme.primaryContainer,
+      child: Center(
+        child: Consumer(
+          builder: (context, ref, child) {
+            // TODO: refactor to use handable states - activeShiftRanges state is acting weird
+            final joinedShifts = ref.watch(curUserJoinedShiftsListenerProvider);
+            final activeShiftRanges = ref.watch(
+                curUserActiveShiftRangesFamProvider((
+              shiftId: joinedShifts?.first.shift.id ?? '',
+              day: DateTime.now().toUtc()
+            )));
+            return switch (joinedShifts) {
+              null => const CircularProgressIndicator(),
+              [] => const Text('No shifts'),
+              List() => activeShiftRanges.isNotEmpty
+                  ? InkWell(
+                      onTap: () => Navigator.of(context).pushNamed(shiftsRoute),
+                      child: _ShiftInnerCard(activeShiftRanges),
+                    )
+                  : const Text('No active shift'),
+            };
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class _ShiftInnerCard extends ConsumerWidget {
+  const _ShiftInnerCard(this.activeShiftRanges);
+
+  final List<DateTimeRange> activeShiftRanges;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Card(
+      color: Theme.of(context).colorScheme.primary,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              DateFormat('EEEE, d MMM').format(DateTime.now()),
+              style: Theme.of(context)
+                  .textTheme
+                  .bodySmall
+                  ?.copyWith(color: Theme.of(context).colorScheme.onSurface),
+            ),
+            const SizedBox(height: 4),
+            Row(
+              children: [
+                Icon(
+                  Icons.calendar_today,
+                  color: Theme.of(context).colorScheme.onSurface,
+                ),
+                const SizedBox(width: 8),
+                Text(
+                  "${DateFormat('H:mm').format(activeShiftRanges.first.start.toLocal())} to ${DateFormat('H:mm').format(activeShiftRanges.first.end.toLocal())}", //"9:00 to 18:00",
+                  style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                      color: Theme.of(context).colorScheme.onSurface),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
