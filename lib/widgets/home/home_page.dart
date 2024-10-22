ff7c7ec3c0a612a4655e2a28e7822adc8673eae7
renata-sisilio <renata@seren-ai.co>
2024-10-22 16:06:13 -0300

    feat(notes): integrate notes home card (#25)

--- lib/widgets/home/home_page.dart
+++ lib/widgets/home/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:seren_ai_flutter/widgets/home/base_home_card.dart';
 import 'package:seren_ai_flutter/widgets/home/clock_in_out_card.dart';
+import 'package:seren_ai_flutter/widgets/home/notes_home_card.dart';
 import 'package:seren_ai_flutter/widgets/home/shift_home_card.dart';
 import 'package:seren_ai_flutter/widgets/home/task_home_card.dart';
 
@@ -45,62 +45,6 @@ class HomePage extends StatelessWidget {
             ],
           );
         },
-      ),
-    );
-  }
-}
-
-Widget NoteCardItem(
-    {required String title,
-    required String subtitle,
-    required BuildContext context}) {
-  return Card(
-    color: Theme.of(context).colorScheme.primary,
-    child: Padding(
-      padding: const EdgeInsets.all(8.0), // Added inner padding
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Text(
-            title,
-            style: Theme.of(context)
-                .textTheme
-                .titleSmall
-                ?.copyWith(fontWeight: FontWeight.bold),
-          ),
-          Text(subtitle, style: Theme.of(context).textTheme.bodySmall),
-        ],
-      ),
-    ),
-  );
-}
-
-class NotesCard extends StatelessWidget {
-  const NotesCard({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return BaseHomeCard(
-      title: "Notes",
-      color: Theme.of(context).colorScheme.primaryContainer,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Flexible(
-            fit: FlexFit.tight,
-            child: NoteCardItem(
-                title: "Meeting Note",
-                subtitle: "Meeting Notes",
-                context: context),
-          ),
-          Flexible(
-            fit: FlexFit.tight,
-            child: NoteCardItem(
-                title: "Site Notes",
-                subtitle: "New roof notations",
-                context: context),
-          ),
-        ],
       ),
     );
   }
