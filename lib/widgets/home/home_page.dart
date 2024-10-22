06cf189ab36808564bd6b6796bd0c76c71747f26
renata-sisilio <renata@seren-ai.co>
2024-10-22 08:20:17 -0300

    feat(shifts): integrate shift-related home cards

--- lib/widgets/home/home_page.dart
+++ lib/widgets/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:seren_ai_flutter/widgets/home/base_home_card.dart';
+import 'package:seren_ai_flutter/widgets/home/clock_in_out_card.dart';
+import 'package:seren_ai_flutter/widgets/home/shift_home_card.dart';
 import 'package:seren_ai_flutter/widgets/home/task_home_card.dart';
 
 class HomePage extends StatelessWidget {
@@ -25,22 +26,20 @@ class HomePage extends StatelessWidget {
                   ),
                 ),
               ),
-              Container(
-                child: ConstrainedBox(
-                  constraints: BoxConstraints(
-                    maxHeight: constraints.maxHeight * 0.6,
-                  ),
-                  child: GridView.count(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 9,
-                    mainAxisSpacing: 9,
-                    children: [
-                      TaskHomeCard(),
-                      NotesCard(),
-                      ShiftCard(),
-                      ClockInOutCard(),
-                    ],
-                  ),
+              ConstrainedBox(
+                constraints: BoxConstraints(
+                  maxHeight: constraints.maxHeight * 0.6,
+                ),
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 9,
+                  mainAxisSpacing: 9,
+                  children: const [
+                    TaskHomeCard(),
+                    NotesCard(),
+                    ShiftCard(),
+                    ClockInOutCard(),
+                  ],
                 ),
               ),
             ],
@@ -102,108 +101,6 @@ class NotesCard extends StatelessWidget {
                 context: context),
           ),
         ],
-      ),
-    );
-  }
-}
-
-class ShiftCard extends StatelessWidget {
-  const ShiftCard({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return BaseHomeCard(
-      title: "Today's Shift",
-      color: Theme.of(context).colorScheme.primaryContainer,
-      child: Center(
-        child: Card(
-          color: Theme.of(context).colorScheme.primary,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisSize: MainAxisSize
-                  .min, // Added to make the column take up only the necessary space
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Monday, 30 Jun",
-                  style: Theme.of(context).textTheme.bodySmall?.copyWith(
-                      color: Theme.of(context).colorScheme.onSurface),
-                ),
-                SizedBox(height: 4),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.calendar_today,
-                      color: Theme.of(context).colorScheme.onSurface,
-                    ),
-                    SizedBox(width: 8),
-                    Text(
-                      "9:00 to 18:00",
-                      style: Theme.of(context).textTheme.bodySmall?.copyWith(
-                          color: Theme.of(context).colorScheme.onSurface),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class ClockInOutCard extends StatelessWidget {
-  const ClockInOutCard({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return BaseHomeCard(
-      title: "Clock in/out",
-      color: Theme.of(context).colorScheme.primaryContainer,
-      child: Center(
-        child: Card(
-          color: Theme.of(context).colorScheme.primary,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.access_time,
-                      color: Theme.of(context).colorScheme.onPrimary,
-                    ),       
-                    SizedBox(width: 8),
-                    TextButton(
-                      onPressed: () {},
-                      child: Text("Start Shift"),
-                      style: TextButton.styleFrom(
-                        foregroundColor: Theme.of(context).colorScheme.onPrimary,
-                        padding: EdgeInsets.zero,
-                      ),
-                    ),
-                  ],
-                ),
-                Text(
-                  "Last logged 9:01",
-                  style: Theme.of(context).textTheme.bodySmall?.copyWith(
-                      color: Theme.of(context).colorScheme.onSurface),
-                ),
-              ],
-            ),
-          ),
-        ),
       ),
     );
   }
