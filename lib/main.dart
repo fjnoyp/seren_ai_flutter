11b9f6256e415dc6c630370d5f3410b55e70fa79
Kyle <kylcheng@gmail.com>
2024-11-08 11:52:40 +0000

    chore(dart): run dart fix apply - cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'dart:ui';
 
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:seren_ai_flutter/app.dart';
--- lib/services/data/common/widgets/form/base_assignees_selection_field.dart
+++ lib/services/data/common/widgets/form/base_assignees_selection_field.dart
@@ -51,6 +51,7 @@ class BaseAssigneesSelectionField extends HookConsumerWidget {
             );
           },
         );
+        return null;
       },
     );
   }
