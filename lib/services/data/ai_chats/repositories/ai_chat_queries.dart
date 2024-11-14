7fe7f82cdfdc957c77ff59391a6b36a7e76a4f33
Kyle <kylcheng@gmail.com>
2024-11-14 14:31:13 +0000

    chore(ai_chat_thread): add paginated loads
    Need to fix the dependency loading logic

--- lib/services/data/ai_chats/repositories/ai_chat_queries.dart
+++ lib/services/data/ai_chats/repositories/ai_chat_queries.dart
@@ -2,10 +2,12 @@ abstract class AiChatQueries {
   /// Params:
   /// - thread_id: String
   static const getThreadMessages = '''
-    SELECT *
-    FROM ai_chat_messages
-    WHERE parent_chat_thread_id = :thread_id
-    ORDER BY created_at DESC;
+  SELECT *
+  FROM ai_chat_messages
+  WHERE parent_chat_thread_id = :thread_id
+  ORDER BY created_at DESC
+  LIMIT :limit
+  OFFSET :offset;
   ''';
 
   /// Params:
