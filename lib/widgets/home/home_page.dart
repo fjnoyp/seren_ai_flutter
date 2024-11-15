706776d01db85b5cd2af2fd5519cc75c877da4cf
renata-sisilio <renata@seren-ai.co>
2024-11-15 16:07:31 -0300

    feat(users): add name and surname to user model
    * show user name at home and at task comments

--- lib/widgets/home/home_page.dart
+++ lib/widgets/home/home_page.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:seren_ai_flutter/services/auth/cur_auth_state_provider.dart';
+import 'package:seren_ai_flutter/services/data/common/widgets/async_value_handler_widget.dart';
 import 'package:seren_ai_flutter/services/data/shifts/widgets/clock_in_out_home_card.dart';
 import 'package:seren_ai_flutter/services/data/notes/widgets/notes_home_card.dart';
 import 'package:seren_ai_flutter/services/data/shifts/widgets/shift_home_card.dart';
 import 'package:seren_ai_flutter/services/data/tasks/widgets/task_home_card.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: SingleChildScrollView(
@@ -20,14 +23,18 @@ class HomePage extends StatelessWidget {
               alignment: Alignment.centerLeft,
               child: Padding(
                 padding: const EdgeInsets.all(30.0),
-                child: Text(
-                  // We decided to use a time greeting to avoid genre issues when using protuguese.
-                  // i.e. Welcome = Bem-vindo (male) / Bem-vinda (female)
-                  AppLocalizations.of(context)!.timeGreeting(
-                    _getTimePeriod(DateTime.now().hour),
-                    'Letty',
+                // Should we use this widget here, since an unauthenticated user will never see this page?
+                child: AsyncValueHandlerWidget(
+                  value: ref.watch(curUserProvider),
+                  data: (user) => Text(
+                    // We decided to use a time greeting to avoid genre issues when using protuguese.
+                    // i.e. Welcome = Bem-vindo (male) / Bem-vinda (female)
+                    AppLocalizations.of(context)!.timeGreeting(
+                      _getTimePeriod(DateTime.now().hour),
+                      user!.firstName,
+                    ),
+                    style: Theme.of(context).textTheme.headlineMedium,
                   ),
-                  style: Theme.of(context).textTheme.headlineMedium,
                 ),
               ),
             ),
