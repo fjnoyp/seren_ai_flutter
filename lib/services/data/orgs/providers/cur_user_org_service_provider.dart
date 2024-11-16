e28a63344f7a53952cc698217cf7e70c69293f54
renata-sisilio <renata@seren-ai.co>
2024-11-16 14:41:05 -0300

    chore: save local preferences per user (language, theme, debug mode, default org)

--- lib/services/data/orgs/providers/cur_user_org_service_provider.dart
+++ lib/services/data/orgs/providers/cur_user_org_service_provider.dart
@@ -3,7 +3,7 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:seren_ai_flutter/services/data/orgs/cur_org_local_key.dart';
 import 'package:seren_ai_flutter/services/data/orgs/providers/cur_org_dependency_provider.dart';
-import 'package:seren_ai_flutter/services/shared_preferences_provider.dart';
+import 'package:seren_ai_flutter/common/shared_preferences_service_provider.dart';
 
 final curUserOrgServiceProvider = Provider<CurUserOrgService>((ref) {
   return CurUserOrgService(ref);
@@ -15,15 +15,13 @@ class CurUserOrgService {
   CurUserOrgService(this.ref);
 
   Future<void> setDesiredOrgId(String desiredOrgId) async {
-    final prefs = ref.read(sharedPreferencesProvider);
-    // TODO p5: desiredOrgId persistency should be per user
+    final prefs = ref.read(sharedPreferencesServiceProvider);
     await prefs.setString(orgIdKey, desiredOrgId);
     ref.refresh(curOrgIdProvider);
   }
 
   Future<void> clearDesiredOrgId() async {
-    final prefs = ref.read(sharedPreferencesProvider);
-    // TODO p5: desiredOrgId persistency should be per user
+    final prefs = ref.read(sharedPreferencesServiceProvider);
     await prefs.remove(orgIdKey);
     ref.refresh(curOrgIdProvider);
   }
