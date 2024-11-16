43d801cd45cee7f53a00f5c876eb33189f872ce3
renata-sisilio <renata@seren-ai.co>
2024-11-16 14:41:05 -0300

    chore: save local preferences per user (language, theme, debug mode, default org)

--- lib/services/shared_preferences_provider.dart
+++
@@ -1,6 +0,0 @@
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final sharedPreferencesProvider = Provider<SharedPreferences>((ref) {
-  throw UnimplementedError('SharedPreferences not overridden');
-});
