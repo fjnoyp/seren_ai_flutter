f93240092b528511a2ce7a542bbde818756eab86
Kyle <kylcheng@gmail.com>
2024-11-18 20:27:20 +0000

    fix(ai): remove pagination, only show last 60 messages

--- lib/services/data/ai_chats/providers/cur_user_ai_chat_messages_provider.dart
+++ lib/services/data/ai_chats/providers/cur_user_ai_chat_messages_provider.dart
@@ -1,9 +1,22 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:seren_ai_flutter/services/data/ai_chats/models/ai_chat_message_model.dart';
+import 'package:seren_ai_flutter/services/data/ai_chats/providers/cur_ai_chat_thread_dependency_provider.dart';
 import 'package:seren_ai_flutter/services/data/ai_chats/providers/cur_user_ai_chat_thread_provider.dart';
 import 'package:seren_ai_flutter/services/data/ai_chats/repositories/ai_chat_messages_repository.dart';
 
-final curUserAiChatMessagesProvider = Provider.autoDispose<AsyncValue<({
+final curUserAiChatMessagesProvider =
+    StreamProvider.autoDispose<List<AiChatMessageModel>>((ref) {
+  return CurAiChatThreadDependencyProvider.watchStream(
+    ref: ref,
+    builder: (aiChatThread) {
+      return ref
+          .watch(aiChatMessagesRepositoryProvider)
+          .watchThreadMessages(threadId: aiChatThread.id ?? '');
+    },    
+  );
+});
+
+final curUserPaginatedAiChatMessagesProvider = Provider.autoDispose<AsyncValue<({
   List<AiChatMessageModel> state,
   AiChatMessagesNotifier notifier
 })>>((ref) {
