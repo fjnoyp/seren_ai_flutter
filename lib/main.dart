c66a94a7a45129c3c3d33034c53d761b07ae2313
renata-sisilio <renata@seren-ai.co>
2024-11-28 08:42:22 -0300

    chore: reactivate Firebase Crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:seren_ai_flutter/app.dart';
@@ -17,15 +18,17 @@ void main() async {
   final db = await PowerSyncDatabaseFactory.openDatabase();
   final prefs = await SharedPreferences.getInstance();
 
-  // // Initialize Firebase for Crashlytics
-  // await Firebase.initializeApp();
-  // // Pass all uncaught "fatal" errors from the framework to Crashlytics
-  // FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
-  // // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
-  // PlatformDispatcher.instance.onError = (error, stack) {
-  //   FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
-  //   return true;
-  // };
+  // if (kReleaseMode) {
+    // Initialize Firebase for Crashlytics
+    await Firebase.initializeApp();
+    // Pass all uncaught "fatal" errors from the framework to Crashlytics
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+    // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+    PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+      return true;
+    };
+  // }
 
   runApp(
     ProviderScope(
