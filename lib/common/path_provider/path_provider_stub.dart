8007e9b701e137922a7ae3827c4a2d2c58a70181
Kyle <kylcheng@gmail.com>
2024-12-03 16:24:35 +0000

    chore(path_provider): split web implementation

---
+++ lib/common/path_provider/path_provider_stub.dart
@@ -0,0 +1,5 @@
+import 'package:seren_ai_flutter/common/path_provider/path_provider.dart';
+
+PathProvider getPathProvider() {
+  throw UnsupportedError('PathProvider is not supported on this platform');
+}
