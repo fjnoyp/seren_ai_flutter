b466b8107ae3ed9e8f91f7aa457ea2b3e0bbcad1
renata-sisilio <renata@seren-ai.co>
2024-12-03 10:59:25 -0300

    feat(users): complete initial setup for new users
    * fixed shared preferences service initialization errors

--- lib/services/data/projects/models/user_project_assignment_model.dart
+++ lib/services/data/projects/models/user_project_assignment_model.dart
@@ -1,12 +1,16 @@
 import 'package:json_annotation/json_annotation.dart';
+import 'package:seren_ai_flutter/services/data/common/i_has_id.dart';
 import 'package:seren_ai_flutter/services/data/common/uuid.dart';
 
 part 'user_project_assignment_model.g.dart';
 
 @JsonSerializable()
-class UserProjectAssignmentModel {
+class UserProjectAssignmentModel implements IHasId {
+  @override
   final String id;
+  @JsonKey(name: 'user_id')
   final String userId;
+  @JsonKey(name: 'project_id')
   final String projectId;
   @JsonKey(name: 'created_at')
   final DateTime? createdAt;
