fcc95e92d329c6baf8d6e1c44b507de7ccf6cecd
renata-sisilio <renata@seren-ai.co>
2024-12-12 15:04:56 -0300

    fix(tasks): localize ai task result widgets

--- lib/services/data/tasks/tool_methods/models/delete_task_result_model.dart
+++ lib/services/data/tasks/tool_methods/models/delete_task_result_model.dart
@@ -4,12 +4,20 @@ class DeleteTaskResultModel extends AiRequestResultModel {
   DeleteTaskResultModel({
     required super.resultForAi,
     required super.showOnly,
+    required this.isDeleted,
+    required this.taskName,
   }) : super(resultType: AiRequestResultType.deleteTask);
+
+  // TODO: remove nullability (supposed to be not null in the backend)
+  final bool? isDeleted;
+  final String? taskName;
 
   factory DeleteTaskResultModel.fromJson(Map<String, dynamic> json) {
     return DeleteTaskResultModel(
       resultForAi: json['result_for_ai'],
       showOnly: json['show_only'],
+      isDeleted: json['is_deleted'],
+      taskName: json['task_name'],
     );
   }
 }
