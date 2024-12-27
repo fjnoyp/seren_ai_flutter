0a01d65aa6cdba15ca6edd67b39cbd54425f6b84
renata-sisilio <renata@seren-ai.co>
2024-12-27 17:25:52 -0300

    feat(settings): move settings to a separated page instead of inside drawer view

---
+++ lib/widgets/settings/settings_page.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:seren_ai_flutter/common/language_provider.dart';
+import 'package:seren_ai_flutter/services/auth/cur_auth_state_provider.dart';
+import 'package:seren_ai_flutter/services/speech_to_text/speech_to_text_service_provider.dart';
+import 'package:seren_ai_flutter/services/text_to_speech/text_to_speech_notifier.dart';
+import 'package:seren_ai_flutter/widgets/common/debug_mode_provider.dart';
+import 'package:seren_ai_flutter/widgets/common/theme_data.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SingleChildScrollView(
+      child: Column(
+        children: [
+          SettingsAccountSection(),
+          SettingsAppSection(),
+        ],
+      ),
+    );
+  }
+}
+
+class SettingsAccountSection extends ConsumerWidget {
+  const SettingsAccountSection({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final user = ref.watch(curUserProvider).value;
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        ListTile(
+          title: Text(AppLocalizations.of(context)!.account),
+        ),
+        user == null
+            ? Center(child: Text(AppLocalizations.of(context)!.noAuthUser))
+            : Column(
+                children: [
+                  // TODO: Add account editing fields here: name, surname, avatar...
+                  ListTile(
+                    dense: true,
+                    leading: const Icon(Icons.email),
+                    title: _BuildRow(
+                        AppLocalizations.of(context)!.email, user.email),
+                  ),
+                  ListTile(
+                    dense: true,
+                    leading: const Icon(Icons.star),
+                    title: _BuildRow(AppLocalizations.of(context)!.subscription,
+                        AppLocalizations.of(context)!.premium),
+                  ),
+                  ListTile(
+                    dense: true,
+                    leading: const Icon(Icons.logout),
+                    title: Text(AppLocalizations.of(context)!.signOut,
+                        style: const TextStyle(fontWeight: FontWeight.bold)),
+                    onTap: () => ref.read(curUserProvider.notifier).signOut(),
+                  ),
+                ],
+              ),
+      ],
+    );
+  }
+}
+
+class SettingsAppSection extends ConsumerWidget {
+  const SettingsAppSection({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final theme = Theme.of(context);
+
+    final themeMode = ref.watch(themeSNP);
+    final language = ref.watch(languageSNP).toUpperCase();
+    final isDebugMode = ref.watch(isDebugModeSNP);
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        ListTile(
+          title: Text(AppLocalizations.of(context)!.app),
+        ),
+        ListTile(
+          dense: true,
+          leading: const Icon(Icons.color_lens),
+          title: Text(AppLocalizations.of(context)!.colorScheme,
+              style: const TextStyle(fontWeight: FontWeight.bold)),
+          trailing: DropdownButton<ThemeMode>(
+            value: themeMode,
+            onChanged: (ThemeMode? newThemeMode) {
+              if (newThemeMode != null) {
+                ref.read(themeSNP.notifier).setTheme(newThemeMode);
+              }
+            },
+            items: [
+              DropdownMenuItem(
+                value: ThemeMode.system,
+                child: Text(AppLocalizations.of(context)!.system,
+                    style: theme.textTheme.bodySmall),
+              ),
+              DropdownMenuItem(
+                value: ThemeMode.light,
+                child: Text(AppLocalizations.of(context)!.light,
+                    style: theme.textTheme.bodySmall),
+              ),
+              DropdownMenuItem(
+                value: ThemeMode.dark,
+                child: Text(AppLocalizations.of(context)!.dark,
+                    style: theme.textTheme.bodySmall),
+              ),
+            ],
+          ),
+        ),
+        ListTile(
+          dense: true,
+          leading: const Icon(Icons.language),
+          title: Text(AppLocalizations.of(context)!.language,
+              style: const TextStyle(fontWeight: FontWeight.bold)),
+          trailing: DropdownButton<String>(
+            value: language,
+            onChanged: (String? newLanguage) {
+              if (newLanguage != null) {
+                ref.read(languageSNP.notifier).setLanguage(newLanguage);
+                ref.read(speechToTextServiceProvider).language = newLanguage;
+                ref.read(textToSpeechServiceProvider.notifier).language =
+                    newLanguage;
+              }
+            },
+            items: [
+              DropdownMenuItem(
+                value: 'EN_US',
+                child: Text(AppLocalizations.of(context)!.english,
+                    style: theme.textTheme.bodySmall),
+              ),
+              DropdownMenuItem(
+                value: 'PT_BR',
+                child: Text(AppLocalizations.of(context)!.brazilianPortuguese,
+                    style: theme.textTheme.bodySmall),
+              ),
+              DropdownMenuItem(
+                value: 'PT_PT',
+                child: Text(AppLocalizations.of(context)!.europeanPortuguese,
+                    style: theme.textTheme.bodySmall),
+              ),
+            ],
+          ),
+        ),
+        SwitchListTile(
+          title: Text(AppLocalizations.of(context)!.debugMode),
+          value: isDebugMode,
+          onChanged: (value) {
+            ref.read(isDebugModeSNP.notifier).setIsDebugMode(value);
+          },
+        ),
+        ListTile(
+          title: Text(AppLocalizations.of(context)!.about),
+        ),
+      ],
+    );
+  }
+}
+
+class _BuildRow extends Row {
+  _BuildRow(String header, String value)
+      : super(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(header, style: const TextStyle(fontWeight: FontWeight.bold)),
+            Text(value),
+          ],
+        );
+}
