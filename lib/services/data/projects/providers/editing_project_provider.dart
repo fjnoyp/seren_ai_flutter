67d97f2074829351d57610ffe9d986f27e405cd0
renata-sisilio <renata@seren-ai.co>
2024-12-30 11:09:49 -0300

    refactor(projects): update cur project state/service provider structure
    refactor(projects): combine state/service providers and remove empty project state
    
    fix(projects): update new project state
    
    feat(projects): auto redirect when project is deleted by other user
    
    * also addressed asked refactorings and fixed state management errors
    
    refactor(projects): clean up code - remove JoinedProjectModel class and it references
    
    refactor: rename providers

---
+++ lib/services/data/projects/providers/editing_project_provider.dart
@@ -0,0 +1,50 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:seren_ai_flutter/services/data/orgs/providers/cur_org_dependency_provider.dart';
+import 'package:seren_ai_flutter/services/data/projects/models/project_model.dart';
+import 'package:seren_ai_flutter/services/data/projects/projects_db_provider.dart';
+
+final editingProjectProvider =
+    NotifierProvider<EditingProjectNotifier, ProjectModel>(() {
+  return EditingProjectNotifier();
+});
+
+class EditingProjectNotifier extends Notifier<ProjectModel> {
+  @override
+  ProjectModel build() {
+    return createNewProject();
+  }
+
+  ProjectModel createNewProject() {
+    final orgId = ref.read(curOrgIdProvider)!;
+    final newProject = ProjectModel(
+      name: 'New Project',
+      description: '',
+      parentOrgId: orgId,
+    );
+    state = newProject;
+    return newProject;
+  }
+
+  Future<void> loadProject(ProjectModel project) async {
+    state = project;
+  }
+
+  /// Returns true if the current editing project is valid to save.
+  bool get isValidProject => state.name.isNotEmpty;
+
+  void updateProject({
+    String? name,
+    String? description,
+    String? address,
+  }) {
+    state = state.copyWith(
+      name: name,
+      description: description,
+      address: address,
+    );
+  }
+
+  Future<void> saveProject() async {
+    await ref.read(projectsDbProvider).upsertItem(state);
+  }
+}
--- lib/services/data/projects/widgets/web/web_project_page.dart
+++ lib/services/data/projects/widgets/web/web_project_page.dart
@@ -3,13 +3,14 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:seren_ai_flutter/services/data/common/status_enum.dart';
 import 'package:seren_ai_flutter/services/data/notes/widgets/notes_list_page.dart';
-import 'package:seren_ai_flutter/services/data/projects/providers/cur_project_state_provider.dart';
+import 'package:seren_ai_flutter/services/data/projects/providers/selected_project_provider.dart';
 import 'package:seren_ai_flutter/services/data/projects/widgets/action_buttons/edit_project_button.dart';
 import 'package:seren_ai_flutter/services/data/projects/widgets/action_buttons/update_project_assignees_button.dart';
 import 'package:seren_ai_flutter/services/data/projects/widgets/project_page.dart';
 import 'package:seren_ai_flutter/services/data/projects/widgets/web/web_project_tasks_section.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:seren_ai_flutter/services/data/tasks/providers/cur_user_viewable_tasks_provider.dart';
+import 'package:seren_ai_flutter/services/data/common/widgets/async_value_handler_widget.dart';
 
 class WebProjectPage extends HookConsumerWidget {
   const WebProjectPage({super.key});
@@ -23,62 +24,64 @@ class WebProjectPage extends HookConsumerWidget {
       ),
       (
         name: AppLocalizations.of(context)!.notes,
-        child:
-            NoteListByProjectId(ref.watch(curProjectStateProvider).project.id)
+        child: NoteListByProjectId(ref.watch(selectedProjectProvider).value!.id)
       ),
     ];
 
     final isProjectInfoView = useState(false);
 
-    return Padding(
-      padding: const EdgeInsets.all(16),
-      child: isProjectInfoView.value
-          ? Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 16),
-                  child: IconButton(
-                    onPressed: () => isProjectInfoView.value = false,
-                    icon: const Icon(Icons.arrow_back),
-                  ),
-                ),
-                const Expanded(child: _WebProjectInfoView()),
-              ],
-            )
-          : DefaultTabController(
-              length: tabs.length,
-              child: Column(
+    return AsyncValueHandlerWidget(
+      value: ref.watch(selectedProjectProvider),
+      data: (selectedProject) => Padding(
+        padding: const EdgeInsets.all(16),
+        child: isProjectInfoView.value
+            ? Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Row(
-                    children: [
-                      Text(
-                        ref.watch(curProjectStateProvider).project.name,
-                        style: Theme.of(context).textTheme.headlineMedium,
-                      ),
-                      IconButton(
-                        onPressed: () => isProjectInfoView.value = true,
-                        color: Theme.of(context).colorScheme.tertiary,
-                        icon: const Icon(Icons.settings),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 8),
-                  const _CurrentProjectReadinessBar(),
-                  const SizedBox(height: 16),
-                  TabBar(
-                    tabs: tabs.map((tab) => Tab(text: tab.name)).toList(),
-                  ),
-                  Expanded(
-                    child: TabBarView(
-                      physics: const NeverScrollableScrollPhysics(),
-                      children: tabs.map((tab) => tab.child).toList(),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16),
+                    child: IconButton(
+                      onPressed: () => isProjectInfoView.value = false,
+                      icon: const Icon(Icons.arrow_back),
                     ),
                   ),
+                  const Expanded(child: _WebProjectInfoView()),
                 ],
+              )
+            : DefaultTabController(
+                length: tabs.length,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          selectedProject.name,
+                          style: Theme.of(context).textTheme.headlineMedium,
+                        ),
+                        IconButton(
+                          onPressed: () => isProjectInfoView.value = true,
+                          color: Theme.of(context).colorScheme.tertiary,
+                          icon: const Icon(Icons.settings),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 8),
+                    const _CurrentProjectReadinessBar(),
+                    const SizedBox(height: 16),
+                    TabBar(
+                      tabs: tabs.map((tab) => Tab(text: tab.name)).toList(),
+                    ),
+                    Expanded(
+                      child: TabBarView(
+                        physics: const NeverScrollableScrollPhysics(),
+                        children: tabs.map((tab) => tab.child).toList(),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
+      ),
     );
   }
 }
@@ -88,7 +91,7 @@ class _CurrentProjectReadinessBar extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final project = ref.watch(curProjectStateProvider).project;
+    final project = ref.watch(selectedProjectProvider).value!;
     final tasks = ref
         .watch(joinedCurUserViewableTasksProvider)
         .valueOrNull
