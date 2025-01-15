15005385e30b3db63b55cc8796ec7d24057107d7
renata-sisilio <renata@seren-ai.co>
2025-01-15 00:20:50 -0300

    fix(projects): filter user viewable projects by org
    * remove unnecesarry queries/methods from repository

--- lib/services/data/projects/providers/cur_user_viewable_projects_provider.dart
+++ lib/services/data/projects/providers/cur_user_viewable_projects_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:seren_ai_flutter/services/data/orgs/providers/cur_selected_org_id_notifier.dart';
 import 'package:seren_ai_flutter/services/data/projects/models/project_model.dart';
 import 'package:seren_ai_flutter/services/data/projects/repositories/projects_repository.dart';
 import 'package:seren_ai_flutter/services/auth/cur_auth_dependency_provider.dart';
@@ -11,7 +12,15 @@ final curUserViewableProjectsProvider =
 
     return CurAuthDependencyProvider.watchStream(
       ref: ref,
-      builder: (userId) => projectsRepo.watchUserProjects(userId: userId),
+      builder: (userId) {
+        final orgId = ref.watch(curSelectedOrgIdNotifierProvider);
+
+        if (orgId == null) {
+          return const Stream.empty();
+        }
+
+        return projectsRepo.watchUserProjects(userId: userId, orgId: orgId);
+      },
     );
   },
 );
