8f2bc5843d699847bec3b94fb2ec8446df88c310
renata-sisilio <renata@seren-ai.co>
2025-01-20 13:49:53 -0300

    feat(tasks): update task action buttons ui for web
    * removed app bar for tasks page on web

--- lib/services/data/tasks/widgets/action_buttons/delete_task_button.dart
+++ lib/services/data/tasks/widgets/action_buttons/delete_task_button.dart
@@ -3,31 +3,48 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:seren_ai_flutter/services/data/common/widgets/delete_confirmation_dialog.dart';
 import 'package:seren_ai_flutter/services/data/tasks/providers/task_by_id_stream_provider.dart';
 import 'package:seren_ai_flutter/services/data/tasks/repositories/tasks_repository.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class DeleteTaskButton extends ConsumerWidget {
-  const DeleteTaskButton(this.taskId, {super.key});
+  const DeleteTaskButton(this.taskId, {super.key, this.showLabelText = false});
 
   final String taskId;
+  final bool showLabelText;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return IconButton(
-      icon: const Icon(Icons.delete),
-      onPressed: () async {
-        final itemName =
-            ref.watch(taskByIdStreamProvider(taskId)).value?.name ?? '';
-        await showDialog(
-          context: context,
-          builder: (context) {
-            return DeleteConfirmationDialog(
-              itemName: itemName,
-              onDelete: () {
-                final tasksRepository = ref.read(tasksRepositoryProvider);
-                tasksRepository
-                    .deleteItem(taskId)
-                    .then((_) => Navigator.of(context).maybePop());
-              },
-            );
+    final redColor = Theme.of(context).colorScheme.error;
+
+    return showLabelText
+        ? OutlinedButton.icon(
+            style: OutlinedButton.styleFrom(
+                foregroundColor: redColor, iconColor: redColor),
+            label: Text(AppLocalizations.of(context)!.deleteTask),
+            icon: const Icon(Icons.delete),
+            onPressed: () async =>
+                await _showDeleteConfirmationDialog(context, ref),
+          )
+        : IconButton(
+            icon: const Icon(Icons.delete),
+            onPressed: () async =>
+                await _showDeleteConfirmationDialog(context, ref),
+          );
+  }
+
+  Future<void> _showDeleteConfirmationDialog(
+      BuildContext context, WidgetRef ref) async {
+    final itemName =
+        ref.watch(taskByIdStreamProvider(taskId)).value?.name ?? '';
+    await showDialog(
+      context: context,
+      builder: (context) {
+        return DeleteConfirmationDialog(
+          itemName: itemName,
+          onDelete: () {
+            final tasksRepository = ref.read(tasksRepositoryProvider);
+            tasksRepository
+                .deleteItem(taskId)
+                .then((_) => Navigator.of(context).maybePop());
           },
         );
       },
