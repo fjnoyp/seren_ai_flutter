11d68106683a27526202ca961abff80142057764
renata-sisilio <renata@seren-ai.co>
2025-01-20 13:49:53 -0300

    feat(tasks): update task action buttons ui for web
    * removed app bar for tasks page on web

--- lib/services/data/tasks/widgets/action_buttons/edit_task_button.dart
+++ lib/services/data/tasks/widgets/action_buttons/edit_task_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:seren_ai_flutter/common/universal_platform/universal_platform.dart';
 import 'package:seren_ai_flutter/services/data/common/widgets/editable_page_mode_enum.dart';
 import 'package:seren_ai_flutter/services/data/tasks/widgets/task_page.dart';
 
@@ -12,6 +13,12 @@ class EditTaskButton extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return IconButton(
+      style: isWebVersion
+          ? IconButton.styleFrom(
+              foregroundColor: Theme.of(context).colorScheme.secondary,
+              iconSize: 18,
+            )
+          : null,
       icon: const Icon(Icons.edit),
       onPressed: () => openTaskPage(
         context,
