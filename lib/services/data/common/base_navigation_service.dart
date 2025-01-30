0fa0553da57e762c7f3166eb2eca2548699732d0
renata-sisilio <renata@seren-ai.co>
2025-01-30 12:43:04 -0300

    chore(navigation): dynamically handle routes to set id when needed
    * implemented abstract BaseNavigationService class
    * fix: remove null check operator from task/note pages' id initialization
    * fix(tasks): task page title

---
+++ lib/services/data/common/base_navigation_service.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:seren_ai_flutter/common/routes/app_routes.dart';
+import 'package:seren_ai_flutter/services/data/notes/providers/notes_navigation_service.dart';
+import 'package:seren_ai_flutter/services/data/projects/providers/project_navigation_service.dart';
+import 'package:seren_ai_flutter/services/data/tasks/providers/task_navigation_service.dart';
+
+abstract class BaseNavigationService {
+  final Ref ref;
+  BaseNavigationService(this.ref);
+
+  NotifierProvider get idNotifierProvider;
+  void setIdFunction(String id);
+
+  static BaseNavigationService? fromAppRoute(AppRoutes route, WidgetRef ref) {
+    return switch (route) {
+      AppRoutes.projectDetails => ref.read(projectNavigationServiceProvider),
+      AppRoutes.taskPage => ref.read(taskNavigationServiceProvider),
+      AppRoutes.notePage => ref.read(notesNavigationServiceProvider),
+      _ => null,
+    };
+  }
+}
