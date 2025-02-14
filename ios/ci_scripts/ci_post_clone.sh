fac1291149f0dbd27ac531316af758b2be9a2f62
Kyle <kylcheng@gmail.com>
2025-02-14 06:04:55 -0800

    fix: switch flutter versions in xcode cloud build
    Latest version of Flutter randomly breaks cloud build:
    https://github.com/flutter/flutter/issues/163198

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -6,9 +6,17 @@ set -e
 # The default execution directory of this script is the ci_scripts directory.
 cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cloned repo.
 
+# OLD - suddenly stopped working: https://github.com/flutter/flutter/issues/163198
 # Install Flutter using git.
-git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
+#git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
+#export PATH="$PATH:$HOME/flutter/bin"
+
+# NEW - Temporary solution?
+# Install Flutter using curl instead of git
+curl -sLO "https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_3.29.0-stable.zip"
+unzip -qq flutter_macos_3.29.0-stable.zip -d $HOME
 export PATH="$PATH:$HOME/flutter/bin"
+
 
 # Install Flutter artifacts for iOS (--ios), or macOS (--macos) platforms.
 flutter precache --ios
