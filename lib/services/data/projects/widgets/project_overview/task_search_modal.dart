5357da1766d418e5b8a6a5af991da39e9cc2122b
renata-sisilio <renata@seren-ai.co>
2025-02-14 15:01:55 -0300

    fix(tasks): prevent adding parent task ids to phases
    * restrict add tasks phase section to tasks that doesn't have a parent task yet
    * remove unnecessary parameter viewMode from task filters widget
    * externalize search modal autofocus

--- lib/services/data/projects/widgets/project_overview/task_search_modal.dart
+++ lib/services/data/projects/widgets/project_overview/task_search_modal.dart
@@ -4,8 +4,9 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'package:seren_ai_flutter/services/ai_interaction/widgets/ai_assistant_button.dart';
 import 'package:seren_ai_flutter/services/data/projects/providers/cur_selected_project_providers.dart';
-import 'package:seren_ai_flutter/services/data/projects/widgets/project_overview/project_tasks_section.dart';
+import 'package:seren_ai_flutter/services/data/projects/task_filter_option_enum.dart';
 import 'package:seren_ai_flutter/services/data/projects/widgets/project_overview/sub_lists/project_tasks_filters.dart';
+import 'package:seren_ai_flutter/services/data/tasks/models/task_model.dart';
 import 'package:seren_ai_flutter/services/data/tasks/providers/task_filter_state_provider.dart';
 import 'package:seren_ai_flutter/services/data/tasks/providers/tasks_by_project_stream_provider.dart';
 import 'package:seren_ai_flutter/services/data/tasks/widgets/task_list/tasks_list_tiles_view.dart';
@@ -13,10 +14,25 @@ import 'package:seren_ai_flutter/services/data/tasks/widgets/task_list/tasks_lis
 // TODO p3: add sort
 // TODO p4: add multi type searching - this should search on notes, shifts, etc. in the future
 class TaskSearchModal extends HookConsumerWidget {
-  const TaskSearchModal({super.key, this.onTapOption});
+  const TaskSearchModal({
+    super.key,
+    this.onTapOption,
+    this.hiddenFilters,
+    this.additionalFilter,
+    this.autoFocus = true,
+  });
 
   /// If this is null, tapping on a task will open the task page
   final void Function(String taskId)? onTapOption;
+
+  /// Filters to hide from the filter list
+  final List<TaskFilterOption>? hiddenFilters;
+
+  /// Additional filter to apply to the tasks (e.g. only show tasks from a certain phase)
+  final bool Function(TaskModel)? additionalFilter;
+
+  /// Whether to auto focus the search field
+  final bool autoFocus;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -40,7 +56,7 @@ class TaskSearchModal extends HookConsumerWidget {
               children: [
                 Expanded(
                   child: TextField(
-                    autofocus: true,
+                    autofocus: autoFocus,
                     onChanged: filterNotifier.updateSearchQuery,
                     decoration: InputDecoration(
                       hintText: AppLocalizations.of(context)!.search,
@@ -62,15 +78,18 @@ class TaskSearchModal extends HookConsumerWidget {
               ],
             ),
             ProjectTasksFilters(
-              viewMode: ProjectTasksSectionViewMode.list,
               onShowCustomDateRangePicker: _showCustomDateRangePicker,
               showExtraViewControls: false,
               useHorizontalScroll: true,
+              hiddenFilters: hiddenFilters,
             ),
             Expanded(
               child: TasksListTilesView(
-                watchedTasks:
-                    ref.watch(tasksByProjectStreamProvider(projectId)),
+                watchedTasks: ref
+                    .watch(tasksByProjectStreamProvider(projectId))
+                    .whenData((tasks) => additionalFilter != null
+                        ? tasks?.where(additionalFilter!).toList()
+                        : tasks),
                 filterCondition: filterState.filterCondition,
                 // TODO p3: add sort
                 //sort: filterState.sortComparator,
