2ec5973291fb03aed973ea84155c3e9f5cd7f473
renata-sisilio <renata@seren-ai.co>
2025-02-18 17:46:01 -0300

    feat(tasks): auto update status on task list page

--- lib/services/data/tasks/widgets/task_list/tasks_list_view.dart
+++ lib/services/data/tasks/widgets/task_list/tasks_list_view.dart
@@ -3,13 +3,20 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:seren_ai_flutter/services/data/common/widgets/async_value_handler_widget.dart';
 import 'package:seren_ai_flutter/services/data/tasks/models/task_model.dart';
 import 'package:seren_ai_flutter/services/data/tasks/providers/cur_user_viewable_tasks_stream_provider.dart';
+import 'package:seren_ai_flutter/services/data/tasks/repositories/tasks_repository.dart';
 import 'package:seren_ai_flutter/services/data/tasks/widgets/task_list/task_list_item_view.dart';
 
 class TasksListView extends ConsumerWidget {
-  const TasksListView({super.key, required this.filter, this.sort});
+  const TasksListView({
+    super.key,
+    required this.filter,
+    this.sort,
+    this.autoUpdateStatusOnDrag = false,
+  });
 
   final bool Function(TaskModel) filter;
   final Comparator<TaskModel>? sort;
+  final bool autoUpdateStatusOnDrag;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -29,9 +36,27 @@ class TasksListView extends ConsumerWidget {
 
         return ListView.builder(
           itemCount: filteredTasks.length,
-          itemBuilder: (context, index) {
-            return TaskListItemView(task: filteredTasks[index]);
-          },
+          itemBuilder: (context, index) =>
+              autoUpdateStatusOnDrag && filteredTasks[index].status != null
+                  ? Dismissible(
+                      key: Key(filteredTasks[index].id),
+                      onDismissed: (direction) {
+                        final curStatus = filteredTasks[index].status!;
+
+                        if (direction == DismissDirection.startToEnd) {
+                          ref.read(tasksRepositoryProvider).updateTaskStatus(
+                                filteredTasks[index].id,
+                                curStatus.nextStatus,
+                              );
+                        } else if (direction == DismissDirection.endToStart) {
+                          ref.read(tasksRepositoryProvider).updateTaskStatus(
+                                filteredTasks[index].id,
+                                curStatus.previousStatus,
+                              );
+                        }
+                      },
+                      child: TaskListItemView(task: filteredTasks[index]))
+                  : TaskListItemView(task: filteredTasks[index]),
         );
       },
     );
