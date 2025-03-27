87f778130cbfbe303dcef9a1fd6cbf46d2334565
Kyle <kylcheng@gmail.com>
2025-03-27 11:06:03 -0700

    chore(ai): add basic note requests + update task request fields

--- lib/services/ai/ai_request/models/requests/ai_info_request_model.dart
+++ lib/services/ai/ai_request/models/requests/ai_info_request_model.dart
@@ -1,12 +1,14 @@
 import 'package:seren_ai_flutter/services/ai/ai_request/models/requests/ai_request_model.dart';
 import 'package:seren_ai_flutter/services/data/shifts/tool_methods/models/shift_request_models.dart';
 import 'package:seren_ai_flutter/services/data/tasks/tool_methods/models/task_request_models.dart';
+import 'package:seren_ai_flutter/services/data/notes/tool_methods/models/note_request_models.dart';
 
 /// Subtypes of Info Request Type
 enum AiInfoRequestType {
   shiftAssignments('shift_assignments'),
   shiftLogs('shift_logs'),
-  findTasks('find_tasks');
+  findTasks('find_tasks'),
+  findNotes('find_notes');
 
   final String value;
   const AiInfoRequestType(this.value);
@@ -42,6 +44,8 @@ class AiInfoRequestModel extends AiRequestModel {
         return ShiftLogsRequestModel.fromJson(json);
       case AiInfoRequestType.findTasks:
         return FindTasksRequestModel.fromJson(json);
+      case AiInfoRequestType.findNotes:
+        return FindNotesRequestModel.fromJson(json);
     }
   }
 
